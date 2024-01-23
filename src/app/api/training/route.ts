ab12f0b75d225ee7aaf14fb4ae027a6d1a63c02e
Luke Boggs <lukeboggs7@gmail.com>
2024-01-23 12:31:11 -0600

    setup gcp auth

--- src/app/api/training/route.ts
+++ src/app/api/training/route.ts
@@ -2,12 +2,28 @@ import Replicate from "replicate";
 import { Storage } from "@google-cloud/storage";
 
 const replicate = new Replicate();
-const storage = new Storage({ projectId: 'vacai-412020' });
+
+if (!process.env.GCP_CREDENTIALS) {
+  throw new Error('GCP_CREDENTIALS not set.');
+}
+
+const credential = JSON.parse(
+  Buffer.from(process.env.GCP_CREDENTIALS, "base64").toString()
+);
+
+const storage = new Storage(
+  {
+    projectId: 'vacai-412020',
+    credentials: {
+      client_email: credential.client_email,
+      private_key: credential.private_key,
+    },
+  }
+);
 
 export async function POST(request: Request) {
 
   const requestBody = await request.json();
-  console.log(requestBody);
 
   const [buckets] = await storage.getBuckets();
   console.log('Buckets:');
