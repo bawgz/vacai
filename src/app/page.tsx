8a24cce3f63d39f5dd89f3677dc22f0cb8a3ed31
Luke Boggs <lukeboggs7@gmail.com>
2024-01-24 11:56:42 -0600

    use vercel blob and upload from client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 'use client'
-import React, { useState } from "react";
+import React, { useState, useRef, FormEvent } from "react";
+import { type PutBlobResult } from '@vercel/blob';
+import { upload } from '@vercel/blob/client';
+import JSZip from "jszip";
 
 export default function Home() {
 
   const [files, setFiles] = useState<FileList | null>(null);
+  const inputFileRef = useRef<HTMLInputElement>(null);
+  const [blob, setBlob] = useState<PutBlobResult | null>(null);
 
   async function handleSubmit(e: any) {
     e.preventDefault();
@@ -27,12 +32,54 @@ export default function Home() {
     return data
   }
 
+  async function uploadFiles(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    console.log(inputFileRef.current!.files);
+
+    if (!inputFileRef?.current?.files || !inputFileRef.current.files.length) {
+      console.error('No files selected');
+      return;
+    }
+    const zip = new JSZip();
+
+    console.log("zipping files");
+    for (const file of inputFileRef.current.files) {
+      zip.file(file.name, Buffer.from(await file.arrayBuffer()))
+    }
+
+    const zipResult = await zip.generateAsync({ type: "arraybuffer" });
+
+    console.log("zip result");
+    console.log(zipResult);
+
+    const trainingId = crypto.randomUUID();
+
+    const newBlob = await upload(`${trainingId}.zip`, zipResult, {
+      access: 'public',
+      handleUploadUrl: '/api/files',
+    });
+
+    console.log("new blob", newBlob);
+
+    setBlob(newBlob);
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <form onSubmit={handleSubmit}>
         <input type="file" accept="image/*" multiple name="file" onChange={(e) => setFiles(e.target.files || null)} />
         <button type="submit">Submit</button>
       </form>
+      <form onSubmit={uploadFiles}>
+        <input name="file" ref={inputFileRef} type="file" accept="image/*" multiple required />
+        <button type="submit">Upload</button>
+      </form>
+      {blob && (
+        <div>
+          Blob url: <a href={blob.url}>{blob.url}</a>
+        </div>
+      )}
     </main>
   )
 }
