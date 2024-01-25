f24e420d2c7160b03ba7681565a886cfba740598
Luke Boggs <lukeboggs7@gmail.com>
2024-01-24 19:15:36 -0600

    add record to postgres after training has started

--- src/app/api/files/route.ts
+++ src/app/api/files/route.ts
@@ -1,8 +1,13 @@
 import { handleUpload, type HandleUploadBody } from '@vercel/blob/client';
 import { NextResponse } from 'next/server';
 import Replicate from "replicate";
+import { sql } from '@vercel/postgres';
 
 const replicate = new Replicate();
+const REPLICATE_BASE_MODEL_OWNER = 'bawgz';
+const REPLICATE_BASE_MODEL = 'dripfusion-base';
+const REPLICATE_BASE_MODEL_VERSION = 'f63d3f0d61f26ce9b2328b12588f8d1e65cc852273606aac29ef42f50a08ae62';
+const REPLICATE_TRAINING_DESTINATION = 'bawgz/dripfusion-trained';
 
 export async function POST(request: Request): Promise<NextResponse> {
   const body = (await request.json()) as HandleUploadBody;
@@ -24,28 +29,55 @@ export async function POST(request: Request): Promise<NextResponse> {
         // Use ngrok or similar to get the full upload flow
 
         console.log('blob upload completed', blob, tokenPayload);
+
+        const trainingInput = {
+          "input_images": blob.url,
+          "caption_prefix": 'A photo of TOK, ',
+          "use_face_detection_instead": true,
+          "train_batch_size": 1,
+          "max_train_steps": 3000,
+          "lora_lr": 1e-4,
+        };
+
         try {
           const trainingResponse = await replicate.trainings.create(
-            'bawgz',
-            'dripfusion-base',
-            'f63d3f0d61f26ce9b2328b12588f8d1e65cc852273606aac29ef42f50a08ae62',
+            REPLICATE_BASE_MODEL_OWNER,
+            REPLICATE_BASE_MODEL,
+            REPLICATE_BASE_MODEL_VERSION,
             {
-              destination: 'bawgz/dripfusion-trained',
-              input: {
-                "input_images": blob.url,
-                "caption_prefix": 'A photo of TOK, ',
-                "use_face_detection_instead": true,
-                "train_batch_size": 1,
-                "max_train_steps": 2000,
-                "lora_lr": 1e-4,
-              }
+              destination: REPLICATE_TRAINING_DESTINATION,
+              input: trainingInput,
             }
           );
 
           console.log('trainingResponse', trainingResponse);
 
+          const baseModel = `${REPLICATE_BASE_MODEL_OWNER}/${REPLICATE_BASE_MODEL}:${REPLICATE_BASE_MODEL_VERSION}`;
+
+          const sqlResult = await sql`
+            INSERT INTO trainings (
+                id, 
+                replicate_id, 
+                base_model,
+                destination_model,
+                input,
+                status
+            ) VALUES (
+                ${blob.pathname.replace('.zip', '')},
+                ${trainingResponse.id},
+                ${baseModel},
+                ${REPLICATE_TRAINING_DESTINATION},
+                ${JSON.stringify(trainingInput)},
+                'pending'
+            );
+          `;
+
+          console.log('sqlResult', sqlResult);
+
+          // use vercel/postgres to store internal id and replicate id and status
+
         } catch (error) {
-          throw new Error('Could not update user');
+          console.error('error', error);
         }
       },
     });
---
+++ src/app/api/trainings/route.ts
@@ -0,0 +1,47 @@
+import { sql } from '@vercel/postgres';
+
+const REPLICATE_BASE_MODEL_OWNER = 'bawgz';
+const REPLICATE_BASE_MODEL = 'dripfusion-base';
+const REPLICATE_BASE_MODEL_VERSION = 'f63d3f0d61f26ce9b2328b12588f8d1e65cc852273606aac29ef42f50a08ae62';
+const REPLICATE_TRAINING_DESTINATION = 'bawgz/dripfusion-trained';
+
+export async function GET() {
+
+  const blob = {
+    pathname: `${crypto.randomUUID()}.zip`,
+    url: 'https://example.com',
+  };
+
+  const trainingInput = {
+    "input_images": blob.url,
+    "caption_prefix": 'A photo of TOK, ',
+    "use_face_detection_instead": true,
+    "train_batch_size": 1,
+    "max_train_steps": 3000,
+    "lora_lr": 1e-4,
+  };
+
+  const baseModel = `${REPLICATE_BASE_MODEL_OWNER}/${REPLICATE_BASE_MODEL}:${REPLICATE_BASE_MODEL_VERSION}`;
+
+  const sqlResult = await sql`
+  INSERT INTO trainings (
+      id, 
+      replicate_id, 
+      base_model,
+      destination_model,
+      input,
+      status
+  ) VALUES (
+      ${blob.pathname.replace('.zip', '')},
+      'wjjkollblo3vby2zvek6oxh63q',
+      ${baseModel},
+      ${REPLICATE_TRAINING_DESTINATION},
+      ${JSON.stringify(trainingInput)},
+      'pending'
+  );
+`;
+
+  console.log('sqlResult', sqlResult);
+
+  return Response.json({ hell: "yeah" })
+}
