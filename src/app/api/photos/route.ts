525b209680e288f53bfa12c6a1e6d93ab28cd335
Luke Boggs <lukeboggs7@gmail.com>
2024-02-05 17:13:05 -0700

    tweak the prompt

--- src/app/api/photos/route.ts
+++ src/app/api/photos/route.ts
@@ -34,12 +34,12 @@ export async function POST(request: Request) {
   const modelParts = data.destination_model.split(':');
 
   const input = {
-    prompt: `((a photo of TOK ${data.class})), standing in front of the pyramids of Giza, wearing bright stylish outfit, instagram`,
+    prompt: `a photo of TOK ${data.class}, standing in front of the pyramids of Giza, wearing bright stylish outfit, instagram`,
     negative_prompt: "((((ugly)))), (((duplicate))), ((morbid)), ((mutilated)), [out of frame], extra fingers, mutated hands, ((poorly drawn hands)), ((poorly drawn face)), (((mutation))), (((deformed))), blurry, ((bad anatomy)), (((bad proportions))), ((extra limbs)), cloned face, (((disfigured))), gross proportions, (malformed limbs), ((missing arms)), ((missing legs)), (((extra arms))), (((extra legs))), (fused fingers), (too many fingers), (((long neck)))",
     refine: "expert_ensemble_refiner",
     high_noise_frac: 0.95,
     lora_scale: 0.8,
-    num_inference_steps: 50,
+    num_inference_steps: 75,
   };
 
   const prediction = await replicate.predictions.create(
