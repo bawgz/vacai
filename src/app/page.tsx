ecaddf8c97a638ed9364cb85063449a5e8b53f73
Luke Boggs <lukeboggs7@gmail.com>
2024-02-13 11:47:32 -0600

    Back to egypt and only load photo form if model available

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -79,21 +79,27 @@ export default function Home() {
       <div className="md:w-1/2 sm:w-full">
         <ModelForm />
       </div>
-      <div className="md:w-1/2 sm:w-full">
-        <h1 className="text-lg">Capture new photos</h1>
-        <form onSubmit={handleTakePhoto}>
-          <select name="models" id="models" ref={modelSelectRef} className="w-full p-2 border border-gray-300 rounded-md text-black my-2">
-            <option value="">Select a model</option>
-            {models.map((model) => (
-              <option key={model.id} value={model.id}>
-                {model.name}
-              </option>
-            ))}
-          </select>
-          <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Capture</button>
-        </form>
-      </div>
-      <PhotoList photos={photos} />
+      {
+        models.length > 0
+        &&
+        <>
+          <div className="md:w-1/2 sm:w-full">
+            <h1 className="text-lg">Capture new photos</h1>
+            <form onSubmit={handleTakePhoto}>
+              <select name="models" id="models" ref={modelSelectRef} className="w-full p-2 border border-gray-300 rounded-md text-black my-2">
+                <option value="">Select a model</option>
+                {models.map((model) => (
+                  <option key={model.id} value={model.id}>
+                    {model.name}
+                  </option>
+                ))}
+              </select>
+              <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Capture</button>
+            </form>
+          </div>
+          <PhotoList photos={photos} />
+        </>
+      }
     </main>
   )
 }
